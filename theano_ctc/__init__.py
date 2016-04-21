47530bf2b0b63c87049df1bb402587326757b77a
Ke Ding <kedingpku@gmail.com>
2016-04-21 15:33:54 +0800

    check device type correctly when gpu is enabled dynamically by sandbox.cuda.use()

--- theano_ctc/__init__.py
+++ theano_ctc/__init__.py
@@ -12,7 +12,7 @@ cdll.LoadLibrary(os.path.join(os.environ["CTC_LIB"], "build", "libwarpctc.so"))
 def ctc_cost(acts, input_lengths, flat_labels, label_lengths):
   # This should be properly integrated into the theano optimization catalog.
   # Until then, this forces the choice based on device configuration.
-  if theano.config.device.startswith("gpu"):
+  if theano.config.device.startswith("gpu") or theano.sandbox.cuda.cuda_enabled:
     if not isinstance(acts.type, CudaNdarrayType): # if not already on the device
       acts = gpu_from_host(acts)  # this should get optimized away
     return gpu_ctc_cost(acts, input_lengths, flat_labels, label_lengths)
